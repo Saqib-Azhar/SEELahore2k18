8b8b130dda0c27e7e02e634c64d68d9e64df72a0
Saqib-Azhar <saqibabdullahazhar@gmail.com>
2018-10-21 01:58:28 +0500

    First Commit

---
+++ SEELahore2k18/SEELahore2k18/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SEELahore2k18.MvcApplication" Language="C#" %>
---
+++ SEELahore2k18/SEELahore2k18/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SEELahore2k18.Startup))]
+namespace SEELahore2k18
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
